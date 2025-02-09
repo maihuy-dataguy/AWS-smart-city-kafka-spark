3df84bcd48af6f6c6d909ab555a87a5665b13d43
Mai Huy <huym@rever.vn>
2025-02-09 20:59:42 +0700

    update config

--- jobs/config/config.py
+++ jobs/config/config.py
@@ -1,5 +1,5 @@
 configuration = {
-    "AWS_ACCESS_KEY": "AKIAUQ4L2Z7CXPCS2FOM",
-    "AWS_SECRET_KEY": "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",
-    "BUCKET_NAME": "smart-city-data-data-engineer"
+    "AWS_ACCESS_KEY": "",
+    "AWS_SECRET_KEY": "",
+    "BUCKET_NAME": ""
 }
